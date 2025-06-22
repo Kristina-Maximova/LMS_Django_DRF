b80daafd8a906690251e6b833b7861e6956a81d3
Kristina Maximova <maximovaki@gmail.com>
2025-06-22 21:20:30 +0300

    removed unnecessary templatetag

--- users/management/commands/create_admin.py
+++ users/management/commands/create_admin.py
@@ -5,7 +5,7 @@ from users.models import LmsUser
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        user = LmsUser.objects.create(email="lsmadmin@example.com")
+        user = LmsUser.objects.create(email="lmsadmin@example.com")
         user.set_password("123456La")
         user.is_active = True
         user.is_staff = True
